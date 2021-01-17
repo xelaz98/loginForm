f2b3fb62a31c42e1a4a68bf3bb39a0933d8c8f86
AlexP <68079031+xelaz98@users.noreply.github.com>
2021-01-17 16:15:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Login System</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+        <form action="login.php" method="post">
+            <h2>Login</h2>
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="error"><?php echo $_GET['error']; ?></p>        
+            <?php } ?>
+            <label>User Name</label>
+            <input type="text" name="uname" placeholder="User Name"><br>
+            <label>User Name</label>
+            <input type="password" name="password" placeholder="Password"><br>
+            
+            <button type="submit">Login</button>
+        </form>
+	</body>
+</html>
